4d08def40b07c9b13c9d7ed7ed41f41d9d360f82
m2 <467057463@qq.com>
2020-06-30 18:31:39 +0800

    change eslint

--- css/.eslintrc.js
+++ css/.eslintrc.js
@@ -1,27 +1,11 @@
 module.exports = {
-    extends: [
-        'alloy',
-        'alloy/react',
-        'alloy/typescript',
-    ],
-    env: {
-        // 您的环境变量（包含多个预定义的全局变量）
-        // Your environments (which contains several predefined global variables)
-        //
-        // browser: true,
-        // node: true,
-        // mocha: true,
-        // jest: true,
-        // jquery: true
-    },
-    globals: {
-        // 您的全局变量（设置为 false 表示它不允许被重新赋值）
-        // Your global variables (setting to false means it's not allowed to be reassigned)
-        //
-        // myGlobal: false
-    },
-    rules: {
-        // 自定义您的规则
-        // Customize your rules
-    }
+  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+  env: {},
+  globals: {},
+  rules: {
+    'capitalized-comments': 'error',
+    camelcase: 'error'
+    // "@typescript-eslint/consistent-type-definitions": ["error", "interface"]
+    // '@typescript-eslint/consistent-type-definitions': 'off'
+  }
 };
--- css/src/index.tsx
+++ css/src/index.tsx
@@ -1,13 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+
+// this is a ppp
+const foo = [1, 2, 3].map((num) => {
+  console.log(num * num);
+});
+
+let my_name = 'mm';
+
+function foos(i: number) {
+  if (i === 1) console.log(i);
+  if (i === 2) console.log(i);
+  if (i === 3) console.log(i);
+  if (i === 4) console.log(i);
+  if (i === 5) console.log(i);
+  if (i === 6) console.log(i);
+  if (i === 7) console.log(i);
+  if (i === 8) console.log(i);
+  if (i === 9) console.log(i);
+  if (i === 10) console.log(i);
+  if (i === 11) console.log(i);
+  if (i === 12) console.log(i);
+  if (i === 13) console.log(i);
+  if (i === 14) console.log(i);
+  if (i === 15) console.log(i);
+  if (i === 16) console.log(i);
+  if (i === 17) console.log(i);
+  if (i === 18) console.log(i);
+  if (i === 19) console.log(i);
+}
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
