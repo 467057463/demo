03d3031925cba9d3782a2b757bd188fda5546993
m2 <467057463@qq.com>
2020-08-21 19:56:41 +0800

    add event

--- js/event/event.js
+++ js/event/event.js
@@ -1,17 +1,56 @@
-var newEvent = new CustomEvent('build', {
-  bubbles:true,
-  cancelable:true,
-  composed:true,
-  detail: {
-    foo: 'bar'
-  }
+// var mmmmmm = 'cnmd';
+// window.addEventListener('load', function(event){
+//   console.log(event)
+//   var script = document.createElement('script');
+//   script.addEventListener('load', function(e){
+//     console.log('load', e)
+//   }, false)
+//   script.src = 'other.js'
+//   document.body.appendChild(script)
+// }, false )
+
+// window.addEventListener('resize', function(event){
+//   console.log(event)
+// }, false)
+
+// window.addEventListener('scroll', function(){
+//   console.log('scroll')
+// }, false)
+
+
+// document.getElementById("input").addEventListener('click', function(){
+//   console.log('click')
+// }, false)
+
+// document.getElementById("input").addEventListener('dblclick', function(){
+//   console.log('dblclick')
+// }, false)
+
+// document.getElementById("input").addEventListener('mousedown', function(event){
+//   console.log('mousedown', event)
+// }, false)
+
+
+// document.getElementById('input').addEventListener('textInput', function(event){
+//   console.log(event)
+//   if(event.data == '1'){
+//     event.preventDefault()
+//     return "123123";
+//   }
+// }, false)
+
+// document.addEventListener('mousewheel', function(event){
+//   console.log(event)
+// }, false)
+
+// document.getElementById('input').addEventListener('keyup', function(event){
+//   console.log(event)
+// }, false)
+
+document.querySelector('#input').addEventListener('compositionstart', function(event){
+  console.log(event)
 })
 
-newEvent.name = '新的事件';
-
-document.addEventListener('build', function(event){
-  alert("sssss")
-  console.log(event)
+document.addEventListener('contextmenu', function(event){
+  event.preventDefault()
 }, false)
-
-document.dispatchEvent(newEvent)
